ab8ba9fec8d5382801a892afb349e2afc4c67c64
Jefry Christiawan <jefrychristiawan2005@gmail.com>
2024-12-18 10:07:31 +0700

    css

--- resources/views/paySuccess.blade.php
+++ resources/views/paySuccess.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('css/paySuccessPage.css')}}">
+    <link rel="stylesheet" href="https://webprogaol-production.up.railway.app/css/paySuccessPage.css">
 </head>
 <body style="background-color: #E5E5E5; overflow:hidden;">
     <div class="container-fluid main-background justify-content-center align-items-center w-100">
