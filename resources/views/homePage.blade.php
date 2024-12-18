96660153b5d24682ae20bd45b009777bab1ec99b
StevenSoeseno <steven.soeseno2004@gmail.com>
2024-12-18 07:19:44 +0700

    cdn bootstrap

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
+{{-- <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}"> --}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script type='module' src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <link rel="stylesheet" href="{{asset('css/homePage.css')}}">
 
